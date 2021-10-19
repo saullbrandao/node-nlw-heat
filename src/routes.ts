6a7a9dd2a1fee4bc262a82258cddbf42cf4af379
Saull Brandão <saull@outlook.com>
2021-10-19 15:07:25 -0300

    feat: create route to get user profile information

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import { AuthenticateUserController } from './controllers/AuthenticateUserController'
 import { CreateMessageController } from './controllers/CreateMessageController'
 import { GetLastThreeMessagesController } from './controllers/GetLastThreeMessagesController'
+import { UserProfileController } from './controllers/UserProfileController'
 import { ensureAuthenticated } from './middleware/ensureAuthenticated'
 
 const router = Router()
@@ -15,5 +16,7 @@ router.post(
 )
 
 router.get('/messages/last3', new GetLastThreeMessagesController().handle)
+
+router.get('/profile', ensureAuthenticated, new UserProfileController().handle)
 
 export { router }
