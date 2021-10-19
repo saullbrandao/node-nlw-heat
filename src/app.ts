d69aae8301f483085fa4dda759972401a6e74ec6
Saull Brandão <saull@outlook.com>
2021-10-19 14:53:07 -0300

    feat: websocket configuration

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,23 @@
 import 'dotenv/config'
 import express from 'express'
+import http from 'http'
+import cors from 'cors'
+import { Server } from 'socket.io'
 import { router } from './routes'
 
 const app = express()
+app.use(cors())
+const serverHttp = http.createServer(app)
+const io = new Server(serverHttp, {
+  cors: {
+    origin: '*',
+  },
+})
+
+io.on('connection', socket => {
+  console.log(`User connected on socket ${socket.id}`)
+})
+
 app.use(express.json())
 app.use(router)
 
@@ -17,4 +32,4 @@ app.get('/signin/callback', (req, res) => {
   return res.json(code)
 })
 
-app.listen(4000, () => console.log('Server is running on port 4000!'))
+export { serverHttp, io }
